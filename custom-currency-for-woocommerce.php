d795e870e7633f23ef22cba39406277a1d5869fc
I am Programmer <iamprogrammerlk@gmail.com>
2020-09-23 20:08:19 +0530

    CHANGE: refactored all the code

--- custom-currency-for-woocommerce.php
+++ custom-currency-for-woocommerce.php
@@ -4,12 +4,12 @@
  * @project
  * Project Name:        Custom Currency For WooCommerce
  * Project Description: Custom Currency For WooCommerce allows you to change the currency symbol used in WooCommerce and you can add a new custom currency type to the WooCommerce.
- * Project Version:     1.1.0
+ * Project Version:     1.1.1
  * File Name:           CustomCurrencyForWooCommerce.php
  * File Description:    This file is read by WordPress to generate the plugin information in the plugin admin area.
  *                      This file also includes all of the dependencies used by the plugin, registers the activation
  *                      and deactivation functions, and defines a function that starts the plugin.
- * File Version:        1.1.0
+ * File Version:        1.1.1
  * Last Change:         2020-09-20
  * 
  * @copyright
@@ -31,7 +31,7 @@
  * Requires PHP:        7.0
  * Text Domain:         CCForWC
  * Domain Path:         /Public/Languages
- * Version:             1.1.0
+ * Version:             1.1.1
  * Author:              I am Programmer
  * Author URI:          https://iamprogrammer.lk
  * License:             GNU GENERAL PUBLIC LICENSE
